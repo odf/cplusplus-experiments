066c40548773b5a7f4bb3234c9ff0fc13b9d8d34
odf <olaf.delgado@gmail.com>
2012-01-31 15:37:20 +1100

    Using lambda expressions in test.cpp.

--- test.cpp
+++ test.cpp
@@ -1,40 +1,6 @@
 #include <iostream>
 #include "LinkedList.hpp"
 
-template<typename T, typename P>
-class Closure
-{
-private:
-    typedef T(*FunPtr)(const P);
-
-    const FunPtr code_;
-    const P arg_;
-public:
-    Closure() :
-        code_(),
-        arg_()
-    {
-    }
-
-    Closure(const FunPtr code, const P arg) :
-        code_(code),
-        arg_(arg)
-    {
-    }
-
-    T operator()() const
-    {
-        return (*code_)(arg_);
-    }
-};
-
-template<typename T, typename P>
-Closure<T, P> closure(T(&code)(P), P arg)
-{
-    return Closure<T, P>(code, arg);
-}
-
-
 int makeInt(const int n)
 {
     std::cout << "makeInt(" << n << ")" << std::endl;
@@ -42,12 +8,12 @@ int makeInt(const int n)
 }
 
 
-typedef odf::LinkedList<int, Closure<int, int> > IntList;
+typedef odf::LinkedList<int> IntList;
 
 int main()
 {
-    IntList one(closure(makeInt, 1));
-    IntList two(closure(makeInt, 2), &one);
+    IntList one([&] () { return makeInt(1); });
+    IntList two([&] () { return makeInt(2); }, &one);
     IntList three(3, &two);
 
     std::cout << three.value() << " "
