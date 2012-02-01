9ac3a8f7f6563bfe6a3432e7f6b6fae28644f877
odf <olaf.delgado@gmail.com>
2012-02-01 14:26:15 +1100

    Test code uses shared list tails.

--- test.cpp
+++ test.cpp
@@ -27,26 +27,17 @@ constant_functor<T> constant(const T val)
 }
 
 
-template<typename F>
-void print_twice(F f, std::string t)
-{
-    std::cout << t << f() << std::endl;
-    std::cout << t << f() << std::endl;
-}
-
-
 int main()
 {
     List<int> one   = makeList<int>(constant(1));
     List<int> two   = makeList<int>(constant(2), one);
     List<int> three = makeList<int>(constant(3), two);
+    List<int> four  = makeList<int>(constant(4), two);
 
     std::cout << three->first() << " "
               << three->rest()->first() << " "
               << three->rest()->rest()->first() << std::endl;
-    std::cout << three->first() << " "
-              << three->rest()->first() << " "
-              << three->rest()->rest()->first() << std::endl;
-
-    print_twice(makeThunk<int>(constant(4)), "thunk() = ");
+    std::cout << four->first() << " "
+              << four->rest()->first() << " "
+              << four->rest()->rest()->first() << std::endl;
 }
