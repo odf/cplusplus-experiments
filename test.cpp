473ec3cdd556a6f16cc1b36cd4e6f99c702a08a5
odf <olaf.delgado@gmail.com>
2012-02-09 17:10:20 +1100

    Function binders, cleaner Fibonacci list, no Boost functors.

--- test.cpp
+++ test.cpp
@@ -1,78 +1,59 @@
+#include <algorithm>
 #include <iostream>
 #include <string>
-#include <boost/lambda/lambda.hpp>
-#include <boost/lambda/bind.hpp>
 #include "Integer.h"
 #include "LinkedList.hpp"
 #include "ListIterator.hpp"
 #include "list_fun.hpp"
 
 using namespace std;
-using namespace boost::lambda;
 using namespace odf;
 
-template<typename T>
-void print(T val, string pre, string post)
+void print(const int val)
 {
-    cout << pre << val << post;
+    cout << "<" << val << ">";
 }
 
-template<typename L>
-struct fibBinder
+int mul(const int lft, const int rgt)
 {
-    fibBinder(const L& list) :
-        list(list)
-    {
-    }
+    return lft * rgt;
+}
 
-    const L operator() () const
-    {
-        return list + list.rest();
-    }
-
-private:
-    const L& list;
-};
-
-template<typename L>
-inline struct fibBinder<L> bindFib(const L& list)
+bool isOdd(const int n)
 {
-    return fibBinder<L>(list);
+    return n % 2 == 1;
+}
+
+List<Integer> fibonacci(const Integer a, const Integer b)
+{
+    return makeList(a, bindFunction(fibonacci, b, a + b));
 }
 
 int main()
 {
     List<int> three = makeList(3, makeList(2, makeList(1)));
-    List<int> four  = makeList(4, constant(three.rest()));
+    List<int> four  = makeList(4, three.rest());
 
     cout << three << endl;
     cout << four << endl;
 
-    forEach(three, bind(print<int>, _1, "<", "> "));
+    forEach(three, print);
     cout << endl;
 
-    std::for_each(ListIterator<int>(four), ListIterator<int>(),
-                  bind(print<int>, _1, "<", "> "));
+    std::for_each(ListIterator<int>(four), ListIterator<int>(), print);
     cout << endl;
 
-    cout << mapList(four, _1 * 2) << endl;
+    cout << mapList(four, curryFunction(mul, 2)) << endl;
 
-    cout << filterList(three, _1 % 2 == 1) << endl;
+    cout << filterList(three, isOdd) << endl;
 
     cout << three + four << endl;
 
     cout << three - four.rest() * three << endl;
 
-    // DISCLAIMER: The following looks nifty, but creates a small memory hole
-    //             due to circular references, which shared_ptr can't handle.
-    List<Integer> fib = makeList(Integer(0), makeList(Integer(1), bindFib(fib)));
+    cout << takeList(listFrom(1000), 10) << endl;
+
+    List<Integer> fib = fibonacci(Integer(0), Integer(1));
     cout << takeList(fib, 10) << endl;
     cout << takeList(dropList(fib, 990), 10) << endl;
-
-    // List<int> fib = makeList(0, makeList(1, bindFib(fib)));
-    // List<int> p = fib;
-    // for (int i = 0; i < 10; ++i, p = p.rest()) {
-    //     cout << " " << p.first();
-    // }
-    // cout << endl;
 }
