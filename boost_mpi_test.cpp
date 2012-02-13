7f670578806b90310403213692d40b6b6d8a8a56
odf <olaf.delgado@gmail.com>
2012-02-13 20:30:59 +1100

    Made the boost::mpi example match the plain MPI one.

--- boost_mpi_test.cpp
+++ boost_mpi_test.cpp
@@ -1,15 +1,79 @@
-#include <boost/mpi/environment.hpp>
-#include <boost/mpi/communicator.hpp>
+#include <boost/mpi.hpp>
 #include <iostream>
+
 namespace mpi = boost::mpi;
+
+template<typename T>
+T add(const T a, const T b)
+{
+    return a + b;
+}
 
 int main(int argc, char* argv[])
 {
-  mpi::environment env(argc, argv);
-  mpi::communicator world;
-  std::cout << "I am process " << world.rank() << " of " << world.size()
-            << "." << std::endl;
-  return 0;
+    int rank, myn, i, N;
+    double *vector, *myvec, mysum, total;
+
+    mpi::environment env(argc, argv);
+    mpi::communicator world;
+
+    rank = world.rank();
+
+    // In the root process read the vector length, initialize
+    // the vector and determine the sub-vector sizes
+    if (rank == 0)
+    {
+        std::cout << "Enter the vector length: " << std::endl;
+        std::cin >> N;
+        vector = new double[N];
+        for (i = 0; i < N; ++i)
+        {
+            vector[i] = i;
+        }
+        myn = N / world.size();
+    }
+
+    // Broadcast the local vector size
+    mpi::broadcast(world, myn, 0);
+
+    // allocate the local vectors in each process
+    myvec = new double[myn];
+
+    // Scatter the vector to all the processes
+    mpi::scatter(world, vector, myvec, myn, 0);
+
+    for(i = 0; i < myn; i++)
+    {
+        std::cout << "[" << rank << "]" << myvec[i] << std::endl;
+    }
+
+    // Find the sum of all the elements of the local vector
+    for (i = 0, mysum = 0; i < myn; i++)
+    {
+        mysum += myvec[i];
+    }
+
+    // Find the global sum of the vectors
+    mpi::all_reduce(world, mysum, total, add<double>);
+
+    // Multiply the local part of the vector by the global sum
+    for (i = 0; i < myn; i++)
+    {
+        myvec[i] *= total;
+    }
+
+    // Gather the local vector in the root process
+    mpi::gather(world, myvec, myn, vector, 0);
+
+    if (rank == 0)
+    {
+        for(i=0; i<N; i++)
+        {
+            std::cout << "[" << rank << "]" << vector[i] << std::endl;
+        }
+    }
+
+    return 0;
 }
 
 /*
