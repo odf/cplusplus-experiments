bc4773af0b66dbedeaf16280dcf5dd7927ec261b
odf <olaf.delgado@gmail.com>
2012-02-13 17:21:33 +1100

    Added a test file for the boost::mpi library.

---
+++ boost_mpi_test.cpp
@@ -0,0 +1,20 @@
+#include <boost/mpi/environment.hpp>
+#include <boost/mpi/communicator.hpp>
+#include <iostream>
+namespace mpi = boost::mpi;
+
+int main(int argc, char* argv[])
+{
+  mpi::environment env(argc, argv);
+  mpi::communicator world;
+  std::cout << "I am process " << world.rank() << " of " << world.size()
+            << "." << std::endl;
+  return 0;
+}
+
+/*
+** Local Variables:
+** mode:C++
+** compile-command: "make -k boost_mpi_test"
+** End:
+*/
