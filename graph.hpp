1fe2a11327bfc048f963372216b37b2b2ec1171e
odf <olaf.delgado@gmail.com>
2012-02-13 23:35:41 +1100

    Started implementing a graph class.

---
+++ graph.hpp
@@ -0,0 +1,70 @@
+#ifndef ODF_GRAPH_HPP
+#define ODF_GRAPH_HPP 1
+
+#include <boost/unordered_set.hpp>
+#include <boost/unordered_map.hpp>
+#include <boost/tuple/tuple.hpp>
+
+#include "list.hpp"
+#include "list_fun.hpp"
+
+
+namespace odf
+{
+
+using boost::unordered_set;
+using boost::unordered_map;
+using std::pair;
+using boost::tie;
+
+template<typename T>
+class Graph
+{
+private:
+    unordered_set<T>                    verts_;
+    unordered_map<T, unordered_set<T> > forw_;
+    unordered_map<T, unordered_set<T> > back_;
+
+public:
+    typedef T          vertex_type;
+    typedef pair<T, T> edge_type;
+
+    template<class Iterator>
+    Graph(Iterator iter, const Iterator end) :
+        verts_(), forw_(), back_()
+    {
+        for(; iter != end; ++iter)
+        {
+            T from, to;
+            tie(from, to) = *iter;
+            addEdge(from, to);
+        }
+    }
+
+    void addEdge(const T from, const T to)
+    {
+        addVertex(from);
+        addVertex(to);
+        forw_[from].insert(to);
+        back_[to].insert(from);
+    }
+
+    void addVertex(const T v)
+    {
+        verts_.insert(v);
+    }
+
+    const int nvertices() const
+    {
+        return verts_.size();
+    }
+
+    const List<vertex_type> vertices() const
+    {
+        return asList(verts_);
+    }
+};
+
+}
+
+#endif // !ODF_GRAPH_HPP
---
+++ graph_test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <utility>
+
+#include "graph.hpp"
+
+using namespace odf;
+
+int main()
+{
+    typedef std::pair<int, int> E;
+
+    E edges[] = { E(1, 2), E(1, 3), E(2, 4), E(3, 4), E(4, 5) };
+
+    Graph<int> G(edges, edges + sizeof(edges) / sizeof(E));
+
+    std::cout << G.vertices() << std::endl;
+}
