24498125a6801295d6982623be120c9fa06c6aec
odf <olaf.delgado@gmail.com>
2012-03-02 23:12:00 +1100

    PersistentMap: use random numbers in timing program.

--- hash_trie_timing.cpp
+++ hash_trie_timing.cpp
@@ -1,5 +1,7 @@
 /* -*-c++-*- */
 
+#include <stdlib.h>
+
 #include "hash_trie.hpp"
 
 using namespace odf::hash_trie;
@@ -13,13 +15,31 @@ typedef PersistentMap<int, int, hashfun> Map;
 
 int main(int argc, char** argv)
 {
-    Map map;
     int const N = atoi(argv[1]);
+    size_t* values = new size_t[N];
+
+    srand(123456789);
 
     for (int i = 0; i < N; ++i)
-        map = map.insert(i, i);
+    {
+        values[i] = rand();
+    }
+
+    Map map;
+
+    for (int i = 0; i < N; ++i)
+        map = map.insert(values[i], i);
+
+    double sum = 0.0;
+    for (int i = N / 2; i < N; ++i)
+    {
+        sum += map.getVal(values[i], 0);
+    }
+
+    std::cout << "RAND_MAX = " << RAND_MAX << std::endl;
+    std::cout << "sum = " << sum << std::endl;
 
     Map copy = map;
-    for (int i = 0; i < N; ++i)
-        copy = copy.remove(i);
+    for (int i = 0; i < N; i += 2)
+        copy = copy.remove(values[i]);
 }
